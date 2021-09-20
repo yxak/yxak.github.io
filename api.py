c63540b498a371512b4c679ea5a66784baa4487c
R_A <75963571+RaADev@users.noreply.github.com>
2021-09-20 12:02:30 +0545

    jow

--- api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-r = requests.get("https://api.github.com/user/yxak")
-
-while True:
-    if r.status_code != 200:
-        jsonned = str(r.json())
-        print(jsonned.capitalize()
